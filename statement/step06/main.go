00aa7d99252abe8d7d47ec12e5d53e25677091bd
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2022-11-21 14:28:48 +0900

    statement step06-2

--- statement/step06/main.go
+++ statement/step06/main.go
@@ -1,19 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
 
 func main() {
-	_, err := os.Open("aaaaa")
+	save()
+	fmt.Println("OK")
+
+	file, err := os.Open("./main.go")
 	if err != nil {
 		log.Fatalln("Exit", err)
 	}
+	defer file.Close()
+	data := make([]byte, 100)
+	// errは上書きで使いまわせる
+	count, err := file.Read(data)
+	if err != nil {
+		log.Fatalln("error")
+	}
+	fmt.Println(count, string(data))
 
 	log.Println("logging")
 	log.Printf("%T", "test")
 
 	// ここでプログラムが終了する
 	log.Fatalln("error")
+
+}
+
+func thirdPartyConnectDB() {
+	// あまり推奨されていない
+	panic("unable")
+}
+
+func save() {
+	// panicをキャッチしてrecoverしている
+	defer func() {
+		s := recover()
+		fmt.Println(s)
+	}()
+	thirdPartyConnectDB()
 }
