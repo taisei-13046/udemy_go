711c84e86eb03d13eaf18a687385a290d80790bb
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2022-11-22 16:55:12 +0900

    struct-oriented step01

---
+++ struct-oriented/step01/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+// func 型名 関数名 返り値
+func (v Vertex) Area() int {
+	return v.X * v.Y
+}
+
+func (v *Vertex) Scale(i int) {
+	v.X = v.X * i
+	v.Y = v.Y * i
+}
+
+func main() {
+	v := Vertex{3, 4}
+	fmt.Println(v.Area())
+
+	v.Scale(10)
+	fmt.Println(v)
+}
