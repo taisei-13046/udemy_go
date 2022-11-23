ece1fe874c89f916c8ad626def5bab3f1ea89249
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2022-11-23 11:25:21 +0900

    struct-oriented step01-3

--- struct-oriented/step01/main.go
+++ struct-oriented/step01/main.go
@@ -17,15 +17,32 @@ func (v *Vertex) Scale(i int) {
 	v.Y = v.Y * i
 }
 
+type Vertex3D struct {
+	// Embeddedで継承できる
+	Vertex
+	Z int
+}
+
+// func 型名 関数名 返り値
+func (v Vertex3D) Area3D() int {
+	return v.X * v.Y * v.Z
+}
+
+func (v *Vertex3D) Scale3D(i int) {
+	v.X = v.X * i
+	v.Y = v.Y * i
+	v.Z = v.Z * i
+}
+
 // コンストラクタ
-func New(x, y int) *Vertex {
-	return &Vertex{x, y}
+func New(x, y, z int) *Vertex3D {
+	return &Vertex3D{Vertex{x, y}, z}
 }
 
 func main() {
-	v := Vertex{3, 4}
-	fmt.Println(v.Area())
+	v := New(3, 4, 5)
+	fmt.Println(v.Area3D())
 
-	v.Scale(10)
-	fmt.Println(v)
+	v.Scale3D(10)
+	fmt.Println(v.Area3D())
 }
