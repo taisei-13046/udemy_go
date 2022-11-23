8bd8435f92445c8e41907126844500b4ba879e26
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2022-11-23 20:08:29 +0900

    gorutine practice

---
+++ gorutine/practice/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func goroutine(s []string, c chan string) {
+	sum := ""
+	for _, v := range s {
+		sum += v
+		c <- sum
+	}
+	close(c)
+}
+
+func main() {
+	words := []string{"test1!", "test2!", "test3!", "test4!"}
+	c := make(chan string)
+	go goroutine(words, c)
+	for w := range c {
+		fmt.Println(w)
+	}
+}
