d97cae06ebf6dbfc295a55cb87ac99631c2d7b9c
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2022-11-24 17:33:12 +0900

    package step01

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/taisei-13046/udemy_go
+
+go 1.19
---
+++ package/step01/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	// 一番親のディレクトリにgo.modを作成してそこを起点にimportする
+	"github.com/taisei-13046/udemy_go/package/step01/mylib"
+)
+
+func main() {
+	s := []int{1, 2, 3, 4, 5}
+	fmt.Println(mylib.Average(s))
+	mylib.Say()
+}
